44c691d5e0cd9a646d28b95cf898ec680965afd8
Siam Ahnaf <siamahnaf198@gmail.com>
2023-02-15 19:35:51 +0600

    Initial Release

---
+++ src/Fonts/index.ts
@@ -0,0 +1,8 @@
+import { Quicksand } from '@next/font/google';
+
+export const rubik = Quicksand({
+    variable: "--font-rubik",
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"],
+    display: 'swap'
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app";
+
+//Fonts
+import { rubik } from "@/Fonts";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${rubik.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
