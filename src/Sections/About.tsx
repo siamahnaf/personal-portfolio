c9d0767599c61bb9544fd48a12f4c06a1bc82dca
Siam Ahnaf <siamahnaf198@gmail.com>
2023-04-18 17:37:40 +0600

    Fixing Apos error in about section

--- src/Sections/About.tsx
+++ src/Sections/About.tsx
@@ -26,7 +26,7 @@ const About = () => {
             <div className="grid grid-cols-2 px-10 py-5 gap-20">
                 <div>
                     <h4 className="text-2xl font-bold mb-3">About <span className="text-main">Me</span></h4>
-                    <p className="opacity-90 mb-3">I'm a MERN stack Web Developer with over 6 years of experience. I'm from Bangladesh. I code and create web elements for amazing people, agencies, and organizations around the world. I like working with new technologies. New Technology is a new Experience.</p>
+                    <p className="opacity-90 mb-3">I&apos;m a MERN stack Web Developer with over 6 years of experience. I&apos;m from Bangladesh. I code and create web elements for amazing people, agencies, and organizations around the world. I like working with new technologies. New Technology is a new Experience.</p>
 
                     <p className="opacity-90">My experience is a coding experience. And my languages/frameworks is also the latest one. My moral is Experience is the key to a Noble work. And passion is the best way to gather more and more Experience!</p>
                     <div className="mt-5">
